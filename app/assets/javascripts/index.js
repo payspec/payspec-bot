911f92d902d26b5f2d495b0f44558b1b74f142ef
andy <andy@jonezie.com>
2020-01-11 20:09:57 -0500

    working homepage

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,75 @@
+
+const $ = require('jquery');
+
+import logo from '../images/senderclogo.png'
+import githubLogo from '../images/GitHub-Mark-64px.png'
+
+import Vue from 'vue'
+
+import AlertRenderer from './alert-renderer'
+
+import HomeRenderer from './home-renderer'
+import GenericDashboard from './generic-dashboard'
+
+import InvoiceRenderer from './invoice-renderer'
+
+
+var url = require('url');
+
+
+var genericDashboard = new GenericDashboard();
+
+
+var alertRenderer = new AlertRenderer();
+var homeRenderer;
+var invoiceRenderer;
+//var invoiceDashboard = new InvoiceDashboard();
+
+
+var navbar = new Vue({
+  el: '#navbar',
+  data: {
+    brandImageUrl: logo,
+    githubLogo: githubLogo
+  }
+})
+
+
+$(document).ready(function(){
+
+    var urlstring = window.location.href ;
+
+    console.log('urlstring',urlstring)
+
+
+          var url_parts = url.parse(urlstring, true);
+          var query = url_parts.query;
+
+          console.log(query)
+
+
+
+      if($("#home").length > 0){
+
+        homeRenderer = new HomeRenderer();
+
+        genericDashboard.init(homeRenderer, query);
+
+
+      }
+
+      if($("#invoice").length > 0){
+
+        invoiceRenderer = new InvoiceRenderer();
+
+        genericDashboard.init(invoiceRenderer, query);
+
+
+      }
+
+
+
+});
+
+
+//dashboardRenderer.hide();
---
+++ webpack.config.js
@@ -0,0 +1,138 @@
+var webpack = require('webpack');
+var path = require('path');
+var MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+var environment = process.env.NODE_ENV || 'development';
+
+
+
+
+// the clean options to use
+let cleanOptions = {
+  verbose:  true,
+  dry:      true,
+  cleanOnceBeforeBuildPatterns: ['/public/*' ],
+}
+
+
+var extractPlugin = new MiniCssExtractPlugin({
+     // Options similar to the same options in webpackOptions.output
+     // both options are optional
+     filename: 'app/assets/main.css'
+   });
+
+
+
+var webpackPlugins = [
+  new CleanWebpackPlugin( cleanOptions),
+    extractPlugin,
+    new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: '"production"'
+        }
+      }),
+
+      new CopyWebpackPlugin([
+            {from:'app/assets/img',to:'app/assets/img'  }
+        ])
+]
+
+
+
+const routesData = {
+  routes: [
+    {url: '/', title: 'SendERC - Token Payment System', template: 'app/index.html', filename: 'index.html'},
+    {url: '/', title: 'SendERC - Invoice', template: 'app/invoice.html', filename: 'invoice.html'}
+
+  //  {url: '/wallet', title: 'Wallet', template: 'app/wallet.html', filename: 'wallet/index.html'},
+  ]
+}
+
+
+routesData.routes.forEach(function(element){
+
+  var htmlPlugin = new HtmlWebpackPlugin({
+        title: element.title,
+        filename: element.filename,
+        template: element.template
+  });
+
+ webpackPlugins.push(htmlPlugin)
+
+})
+
+
+
+module.exports = {
+    entry: ['./app/assets/javascripts/index', './app/assets/stylesheets/application.scss' ],
+    output: {
+        path: path.resolve(__dirname, 'public'),
+      //  filename: 'bundle.js',
+        filename: '[name].[hash].js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+          {
+             test: /\.m?js$/,
+             exclude: /(node_modules|bower_components)/,
+             use: {
+               loader: 'babel-loader',
+               options: {
+                 presets: ['@babel/preset-env']
+               }
+             }
+           },
+            {
+                test: /\.scss$/,
+                use: [
+               {
+                 loader: MiniCssExtractPlugin.loader,
+                 options: {
+
+                 },
+               },
+               'css-loader','sass-loader'
+             ],
+            },
+            {
+              test: /\.(png|jpg|gif)$/,
+              use: [
+                {
+                  loader: 'file-loader',
+                  options: {
+                    name: '[path][name].[ext]',
+                     publicPath: '/',
+                  }
+                }
+              ]
+            },
+
+            {
+              test: /\.(eot|woff|woff2|ttf|svg)(\?[\s\S]+)?$/,
+              use: [
+                {
+                  loader: 'file-loader',
+                  options: {
+                    name: '[path][name].[ext]',
+                     publicPath: '/',
+                  }
+                }
+              ]
+            },
+        ]
+    },
+    resolve: {
+      alias: {
+        'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
+      }
+    },
+    plugins: webpackPlugins
+};
