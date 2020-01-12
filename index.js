b6645acd5943cad866400d4165e2e4f9f49bd4c1
andy <andy@jonezie.com>
2020-01-12 02:34:29 -0500

    serving up

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log('hi')
+
+
+var mongoInitParam;
+
+var mongoInterface = require('./lib/mongo-interface')
+
+var peerInterface = require('./lib/peer-interface')
+
+
+init();
+
+
+async function init()
+{
+  await mongoInterface.init(mongoInitParam)
+
+  await peerInterface.init(mongoInterface) //initJSONRPCServer();
+}
