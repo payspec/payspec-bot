f8e4f90080688d6cf948114d75371d225c8ccc0c
andy <andy@jonezie.com>
2020-01-12 23:29:28 -0500

    comments

--- sample.account.config.js
+++ sample.account.config.js
@@ -1,14 +1,15 @@
 var accounts = {
 
-  minting: {
+  //ethereum address where money from paid invoices will be deposited
+  recipient: {
+    address: '0xf3243bAbF74Ead828ac656877137Df705868fD66'
+  },
+
+  //ethereum account that will execute onchain Ethereum methods
+  msgsender: {
     address: '0xf3243bAbF74Ead828ac656877137Df705868fD66',
     privateKey: '00000'
   },
-
-  payment: {
-    address: '0xf3243bAbF74Ead828ac656877137Df705868fD66',
-    privateKey: '00000'
-  }
 
 }
 
